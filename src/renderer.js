6fcb783ccf700d0ca5553019851ee8666134c14f
Ashley Chapman <xpsx.fan@googlemail.com>
2026-02-04 01:23:39 +0000

    Improved ContextMenu, Added Debug Tools if Debugging in VSCode/NPM, upgraded Electron to v40

--- src/renderer.js
+++ src/renderer.js
@@ -3,7 +3,11 @@ console.log('Renderer script running');
 document.addEventListener('contextmenu', (e) => {
   e.preventDefault();
   if (typeof window.electronAPI?.showContextMenu === 'function') {
-    window.electronAPI.showContextMenu();
+    const selection = window.getSelection().toString();
+
+    window.electronAPI.showContextMenu({
+      selectionText: selection
+    });
   } else {
     console.error('showContextMenu is not available on electronAPI');
   }
